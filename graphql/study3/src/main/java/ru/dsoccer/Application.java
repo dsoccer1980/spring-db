cc5b1f0095e70db6ab2d2316271e660eafb15cf1
deniss.vorosnin <deniss.vorosnin@aktors.ee>
2021-03-22 14:52:18 +0200

    Add ElementCollection

--- graphql/study3/src/main/java/ru/dsoccer/Application.java
+++ graphql/study3/src/main/java/ru/dsoccer/Application.java
@@ -1,13 +1,31 @@
 package ru.dsoccer;
 
+import javax.persistence.EntityGraph;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import org.hibernate.graph.GraphSemantic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.dsoccer.model.Car;
+import ru.dsoccer.repository.CarRepository;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class);
+    ConfigurableApplicationContext context = SpringApplication.run(Application.class);
+
+    CarRepository bean = context.getBean(CarRepository.class);
+    EntityManagerFactory managerFactory = context.getBean(EntityManagerFactory.class);
+    EntityManager em = managerFactory.createEntityManager();
+    EntityGraph<Car> entityGraph = em.createEntityGraph(Car.class);
+    entityGraph.addAttributeNodes("details");
+    TypedQuery<Car> query = em.createQuery("Select c from Car c ", Car.class);
+    query.setHint(GraphSemantic.FETCH.getJpaHintName(), entityGraph);
+    System.out.println(query.getResultList());
+
   }
 
 }
--- graphql/study3/src/main/java/ru/dsoccer/model/Car.java
+++ graphql/study3/src/main/java/ru/dsoccer/model/Car.java
@@ -1,6 +1,8 @@
 package ru.dsoccer.model;
 
 import io.leangen.graphql.annotations.GraphQLQuery;
+import java.util.List;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,5 +20,8 @@ public class Car {
 
   @GraphQLQuery(name = "name", description = "A name")
   private String name;
+
+  @ElementCollection
+  private List<String> details;
 
 }
